6fe6445f507e43c04bfe08163777d57a1f7c701e
gracgc <61702739+gracgc@users.noreply.github.com>
2021-04-19 19:07:47 +0300

    123

--- routes/devices.routes.js
+++ routes/devices.routes.js
@@ -75,11 +75,11 @@ router.put('/add', cors(), function (req, res) {
         }
 
 
-        socket.to(stadium).emit(`getDevices`, DB.devices)
+        io.to(stadium).emit(`getDevices`, DB.devices)
 
         console.log(123)
 
-        socket.to(stadium).emit(`getLag${socket.id}`, DB.devices[DB.devices.findIndex(user => user.id === socket.id)].lag)
+        io.to(stadium).emit(`getLag${socket.id}`, DB.devices[DB.devices.findIndex(user => user.id === socket.id)].lag)
 
 
         let json = JSON.stringify(DB);

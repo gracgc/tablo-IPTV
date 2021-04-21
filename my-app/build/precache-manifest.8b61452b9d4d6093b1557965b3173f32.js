dac390e35749494112ce97f93970f804e4f8343a
gracgc <61702739+gracgc@users.noreply.github.com>
2021-04-21 20:56:51 +0300

    123

---
+++ my-app/build/precache-manifest.8b61452b9d4d6093b1557965b3173f32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867e9bf333938f6f31a0c34c01607050",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b479f0bdcd58a09c4fd4",
+    "url": "/static/css/main.1d2a4b60.chunk.css"
+  },
+  {
+    "revision": "63515f392a646d7a5e80",
+    "url": "/static/js/2.ae3b3b48.chunk.js"
+  },
+  {
+    "revision": "793d54e67d44b21720550a1d7c35cc9d",
+    "url": "/static/js/2.ae3b3b48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b479f0bdcd58a09c4fd4",
+    "url": "/static/js/main.9df817cf.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "2c5cd2ef6f6b690ccc564b29c376a895",
+    "url": "/static/media/logoIPTVPORTAL.2c5cd2ef.png"
+  }
+]);

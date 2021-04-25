0f90b8d6a580d361a8b2efce4803b0bfbfc2eabf
gracgc <61702739+gracgc@users.noreply.github.com>
2021-04-25 19:35:03 +0300

    123

--- my-app/build/precache-manifest.8abf26d6ba97063677193ee8ec5c51a3.js
+++ my-app/build/precache-manifest.8abf26d6ba97063677193ee8ec5c51a3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e04be258f3cc4f09c259c6c9d8a498c0",
+    "revision": "3ae849475253f9246be103aab81f8c94",
     "url": "/index.html"
   },
   {
-    "revision": "e2a1f26de9bf784bc638",
+    "revision": "783d683b5db98edaa596",
     "url": "/static/css/main.dd549de7.chunk.css"
   },
   {
-    "revision": "f329d2223c2dd509fd99",
-    "url": "/static/js/2.69afbcc6.chunk.js"
+    "revision": "c2ed417e399c2fbc6638",
+    "url": "/static/js/2.a59ac757.chunk.js"
   },
   {
     "revision": "793d54e67d44b21720550a1d7c35cc9d",
-    "url": "/static/js/2.69afbcc6.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.a59ac757.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e2a1f26de9bf784bc638",
-    "url": "/static/js/main.4a8c08e6.chunk.js"
+    "revision": "783d683b5db98edaa596",
+    "url": "/static/js/main.bea26dc9.chunk.js"
   },
   {
     "revision": "73635f645c271e35ff15",
